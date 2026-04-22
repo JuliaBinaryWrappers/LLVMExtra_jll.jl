840ac017071e40cae048518a5ce770cac64812ca
jlbuild <juliabuildbot@gmail.com>
2026-04-22 09:14:33 +0000

    LLVMExtra_jll build 0.0.39+0

--- src/LLVMExtra_jll.jl
+++ src/LLVMExtra_jll.jl
@@ -7,5 +7,5 @@ Base.include(@__MODULE__, joinpath("..", ".pkg", "platform_augmentation.jl"))
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("LLVMExtra")
-JLLWrappers.@generate_main_file("LLVMExtra", UUID("dad2f222-ce93-54a1-a47d-0025e8a3acab"))
+JLLWrappers.@generate_main_file("LLVMExtra", Base.UUID("dad2f222-ce93-54a1-a47d-0025e8a3acab"))
 end  # module LLVMExtra_jll
